40775299495063250660daae5a4663a729c627a3
Derek Prieur <DPrieur123@gmail.com>
2023-10-12 10:40:40 -0400

    updates

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button";
+import Footer from "@/components/marketing/Footer";
+import Heading from "@/components/marketing/Heading";
+import Heroes from "@/components/marketing/Heroes";
 
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex min-h-full flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center md:justify-start">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
     </div>
   );
-}
+};
+
+export default MarketingPage;
---
+++ components/marketing/Heroes.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const Heroes = () => {
+  return (
+    <div className="flex max-w-5xl flex-col items-center justify-center">
+      <div className="flex items-center">
+        <div className="relative h-[300px] w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            src="/documents.png"
+            fill
+            alt="documents"
+            className="object-contain"
+          />
+        </div>
+        <div className="relative hidden h-[400px] w-[400px] md:block">
+          <Image
+            src="/reading.png"
+            fill
+            className="object-contain"
+            alt="reading"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heroes;
