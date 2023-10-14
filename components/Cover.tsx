1a6ad948c0ad0558e67d479ef53c9fe41f266059
Derek Prieur <DPrieur123@gmail.com>
2023-10-14 18:43:50 -0400

    updates

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -2,6 +2,15 @@
 
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Button } from "./ui/button";
+import { ImageIcon, X } from "lucide-react";
+import { useCoverImage } from "@/hooks/useCoverImage";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useParams } from "next/navigation";
+import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
+import { Skeleton } from "./ui/skeleton";
 
 interface CoverProps {
   url?: string;
@@ -9,6 +18,22 @@ interface CoverProps {
 }
 
 const Cover = ({ preview, url }: CoverProps) => {
+  const params = useParams();
+  const coverImage = useCoverImage();
+  const { edgestore } = useEdgeStore();
+  const removeCoverImage = useMutation(api.documents.removeCoverImage);
+
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url,
+      });
+    }
+    removeCoverImage({
+      id: params.documentId as Id<"documents">,
+    });
+  };
+
   return (
     <div
       className={cn(
@@ -18,8 +43,34 @@ const Cover = ({ preview, url }: CoverProps) => {
       )}
     >
       {!!url && <Image src={url} alt="cover" fill className="object-cover" />}
+      {url && !preview && (
+        <div className="absolute bottom-5 right-5 flex items-center gap-x-2 opacity-0 group-hover:opacity-100">
+          <Button
+            onClick={() => coverImage.onReplace(url)}
+            className="text-xs text-muted-foreground"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="mr-2 h-4 w-4" />
+            Change cover
+          </Button>
+          <Button
+            onClick={onRemove}
+            className="text-xs text-muted-foreground"
+            variant="outline"
+            size="sm"
+          >
+            <X className="mr-2 h-4 w-4" />
+            Remove
+          </Button>
+        </div>
+      )}
     </div>
   );
+};
+
+Cover.Skeleton = function CoverSkeleton() {
+  return <Skeleton className="h-[12vh] w-full" />;
 };
 
 export default Cover;
--- components/modals/CoverImageModal.tsx
+++ components/modals/CoverImageModal.tsx
@@ -30,7 +30,12 @@ const CoverImageModal = () => {
       setIsSubmitting(true);
       setFile(file);
 
-      const res = await edgestore.publicFiles.upload({ file });
+      const res = await edgestore.publicFiles.upload({
+        file,
+        options: {
+          replaceTargetUrl: coverImage.url,
+        },
+      });
 
       await update({
         id: params.documentId as Id<"documents">,
